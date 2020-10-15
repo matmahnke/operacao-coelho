bf042ab9fa39a678e44c33fa6e85945c19649013
Matheus Mahnke <matheusmahnke@gmail.com>
2020-10-15 20:42:17 -0300

    avaliacao 1

---
+++ CODIGO-FONTE/server/public/js/game.js
@@ -0,0 +1,117 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 1800,
+    height: 1800,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('ship', 'assets/dog-spaceship.png');
+    this.load.image('otherPlayer', 'assets/rabbit-spaceship.png');
+    this.load.image('star', 'assets/meteor.png');
+}
+
+function create() {
+    var self = this;
+    this.socket = io();
+    this.players = this.add.group();
+
+    this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
+    this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
+
+
+    // game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    // game.world.setBounds(0, 0, 1920, 1920);
+    // game.physics.startSystem(Phaser.Physics.P2JS);
+    // game.camera.follow(players.filt);
+
+    this.socket.on('currentPlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+            if (players[id].playerId === self.socket.id) {
+                displayPlayers(self, players[id], 'ship');
+            } else {
+                displayPlayers(self, players[id], 'otherPlayer');
+            }
+        });
+    });
+
+    this.socket.on('newPlayer', function (playerInfo) {
+        displayPlayers(self, playerInfo, 'otherPlayer');
+    });
+
+    this.socket.on('disconnect', function (playerId) {
+        self.players.getChildren().forEach(function (player) {
+            if (playerId === player.playerId) {
+                player.destroy();
+            }
+        });
+    });
+
+    this.socket.on('playerUpdates', function (players) {
+        Object.keys(players).forEach(function (id) {
+            self.players.getChildren().forEach(function (player) {
+                if (players[id].playerId === player.playerId) {
+                    player.setRotation(players[id].rotation);
+                    player.setPosition(players[id].x, players[id].y);
+                }
+            });
+        });
+    });
+
+    this.socket.on('updateScore', function (scores) {
+        self.blueScoreText.setText('aliados: ' + scores.blue);
+        self.redScoreText.setText('inimigos: ' + scores.red);
+    });
+
+    this.socket.on('starLocation', function (starLocation) {
+        if (!self.star) {
+            self.star = self.add.image(starLocation.x, starLocation.y, 'star');
+        } else {
+            self.star.setPosition(starLocation.x, starLocation.y);
+        }
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.leftKeyPressed = false;
+    this.rightKeyPressed = false;
+    this.upKeyPressed = false;
+}
+
+function update() {
+    const left = this.leftKeyPressed;
+    const right = this.rightKeyPressed;
+    const up = this.upKeyPressed;
+
+    if (this.cursors.left.isDown) {
+        this.leftKeyPressed = true;
+    } else if (this.cursors.right.isDown) {
+        this.rightKeyPressed = true;
+    } else {
+        this.leftKeyPressed = false;
+        this.rightKeyPressed = false;
+    }
+
+    if (this.cursors.up.isDown) {
+        this.upKeyPressed = true;
+    } else {
+        this.upKeyPressed = false;
+    }
+
+    if (left !== this.leftKeyPressed || right !== this.rightKeyPressed || up !== this.upKeyPressed) {
+        this.socket.emit('playerInput', { left: this.leftKeyPressed, right: this.rightKeyPressed, up: this.upKeyPressed });
+    }
+}
+
+
+function displayPlayers(self, playerInfo, sprite) {
+    const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    player.playerId = playerInfo.playerId;
+    self.players.add(player);
+}
